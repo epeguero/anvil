873dfafbc467f50f267dac017a93f12e4a25e6f1
Edwin Peguero <edwinmpeguero@gmail.com>
2023-09-08 20:40:06 -0400

    ts-based module creation

--- app/sandpack-sandbox/page.tsx
+++ app/sandpack-sandbox/page.tsx
@@ -1,15 +1,70 @@
 'use client';
 
 import React from 'react';
-import {SandpackCodeEditor, SandpackLayout, SandpackPreview, SandpackProvider} from "@codesandbox/sandpack-react";
+import {SandpackCodeEditor, SandpackLayout, SandpackPreview, SandpackProvider, SandpackConsumer, SandpackReactContext, SandpackContext} from "@codesandbox/sandpack-react";
+import { EditorView, ViewUpdate } from "@codemirror/view"
+import * as ts from "typescript";
 
-export default function Home() {
-  return (<Sandbox/>)
+function printAst(ast: ts.Node) {
+  return ts.createPrinter().printNode(
+      ts.EmitHint.Unspecified, 
+      ast,
+      undefined as any
+  )
 }
 
-function Sandbox() {
+function createModule(name: string, children: string[]) {
+  return ts.factory.createModuleDeclaration(
+    [],
+    ts.factory.createIdentifier(name),
+    ts.factory.createModuleBlock(
+      [
+        ts.factory.createExportDefault(
+          ts.factory.createFunctionExpression(
+            [],
+            undefined,
+            ts.factory.createIdentifier(name),
+            [],
+            [],
+            undefined,
+            ts.factory.createBlock(
+              [
+                ts.factory.createReturnStatement(
+                  ts.factory.createParenthesizedExpression(
+                    ts.factory.createJsxFragment(
+                      ts.factory.createJsxOpeningFragment(),
+                      [],
+                      ts.factory.createJsxJsxClosingFragment()
+                    )
+                  )
+                )
+              ],
+              true
+            )
+          )
+        )
+      ]
+    ),
+    undefined,
+  );
+}
+
+export default function Home() {
   return (
-    <SandpackProvider 
+    <Sandbox elementName={"MyComponent"} />
+  )
+}
+
+function Sandbox({
+  elementName
+}: {
+  elementName: string
+}) {
+  return (
+    <SandpackProvider
+      options ={{
+        visibleFiles: [`/${elementName}.tsx`, '/package.json']
+      }}
       customSetup = {
         {
           dependencies: {
@@ -19,50 +74,51 @@ function Sandbox() {
       }
       files = {
         { 
-          // We infer dependencies and the entry point from package.json 
+          [`/${elementName}.tsx`] : { 
+            code: printAst(emptyModule(elementName))
+          },
           "/package.json": {
             code: JSON.stringify(
-                {
-                  main: "index.js",
-                  dependencies: { 
-                    'react': "latest",
-                    'react-dom': "latest"
-                  },
-                }
-              )
+              {
+                main: "index.js",
+                dependencies: { 
+                  'react': "latest",
+                  'react-dom': "latest"
+                },
+              }
+            )
           },
-          
+
           // Main file
           "/index.js": { 
             code: 
-`import ReactDOM from 'react-dom';
-import A from './A.js';
+`import ReactDOM from 'react-dom/client';
+import ${elementName} from './${elementName}.tsx';
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<A/>);
+  .render(<${elementName}/>);
 ` 
-          },
-
-           "/A.js": {
-            code:
-`import React, {useState} from 'react';
-export default function A() {
-  const [n, setN] = useState(0);
-  return (
-    <>
-      <button onClick={() => setN(n+1)}>Increment</button>
-      {n}
-    </>
-  );
-}
-`
           }
-
         }
       }
     >
       <SandpackLayout>
-        <SandpackCodeEditor />
+        <SandpackConsumer>
+        { (ctx: SandpackContext | null) => (
+            <SandpackCodeEditor 
+              // ref={codeMirrorInstance} 
+              // initMode="immediate"
+              extensions={[
+                EditorView.updateListener.of(
+                  (update: ViewUpdate) => {
+                    console.log(ctx);
+                  }
+                )
+              ]}
+            />
+          )
+        }
+        </SandpackConsumer>
         <SandpackPreview showOpenInCodeSandbox={false}/>
       </SandpackLayout>
     </SandpackProvider>
